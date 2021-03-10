b14e55a2e8fb039a2afb8b837f60d9a5d5610762
Tung Poems <tungdd.hrt@gmail.com>
2021-03-11 01:37:26 +0700

    Limitless

--- Limitless.cpp
+++ Limitless.cpp
@@ -20,16 +20,44 @@ void print_menu()
 }
 void print_instructions()
 {
+    char is_back;
     cout<<"INSTRUCTIONS"<<endl<<endl;
     cout<<"First of all,You have to choose 2 numbers as min and max.Then the computer will give you a number in range between those number.This is your start number."<<endl;
     cout<<"Rules:"<<endl;
     cout<<"Each turn,The computer will give you 2 numbers randomly.You have to choose one which will be added to your start number."<<endl;
-    cout<<"REMEMBER: You have to pick before the time runs out to keep your start number in range between min and max or you will lose,so choose wisely"<<endl<<endl;
+    cout<<"REMEMBER: You have to pick before the time runs out to keep your start number in range between min and max or you will lose,so choose wisely."<<endl<<endl;
     cout<<"- Have fun - "<<endl<<endl;
+    cout<<"Back to menu? (y/n)"<<endl;
+    cin>>is_back;
+    if(is_back=='n'||is_back=='N')
+    {
+        print_instructions();
+    }
 }
 
 int main()
 {
-    print_menu();
+    bool return_menu=false;
+    int choose;
+    do
+    {
+        print_menu();
+        cin>>choose;
+        switch (choose)
+        {
+        case 1:
+            print_instructions();
+            break;
+        case 2:
+            choose_mode();
+            break;
+        case 3:
+            game_loop();
+            break;
+        default:
+            cout<<"Error: Invalid choice."<<endl;
+            break;
+        }
+    } while (!return_menu);
     return 0;
 }
